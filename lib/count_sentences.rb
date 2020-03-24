484e589cbb747223be6dea1422f763d585d138ab
Zaire  Swailes <swaileszaire@gmail.com>
2020-03-24 08:32:52 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+  self.end_with?(".")
   end
 
   def question?
-
+  self.end_with?("?")
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
-
+  
   def count_sentences
-
+    self.split(".", "?", "!").reject {|x| x.empty?}.size
   end
+  
+  def count_sentences
+   self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
 end
+end
+    
