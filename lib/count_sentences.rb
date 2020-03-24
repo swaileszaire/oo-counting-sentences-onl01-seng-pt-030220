156a4d5cf959a898fd56f8191058962ba3cd4ff0
Zaire  Swailes <swaileszaire@gmail.com>
2020-03-24 07:42:21 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+  self.end_with?(".")
   end
 
   def question?
-
+  self.end_with?("?")
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
 
   def count_sentences
-
+    array = []
+    count = 0 
+    count += 1 
+      self.split(".", "?", "!").uniq 
+    break if count < 10
   end
-end
+  
+    
